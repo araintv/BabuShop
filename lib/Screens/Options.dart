5c583822aecc07c54bbac1b2d6b93b7467ddba9e
araintv <araintv786@gmail.com>
2025-05-08 11:53:10 +0500

    optimize
    optimize

--- lib/Screens/Options.dart
+++ lib/Screens/Options.dart
@@ -41,20 +41,22 @@ class _OptionsScreenState extends State<OptionsScreen> {
 
   Future<void> fetchBalances() async {
     final data = await UserSheetsApi.fetchAllRows();
-
-    Map<String, double> newBalances = Map.from(accountBalances);
+    final accountKeys = accountBalances.keys.toSet();
+    Map<String, double> newBalances =
+        Map.fromIterable(accountKeys, value: (_) => 0.0);
 
     for (var row in data.skip(1)) {
-      String jama = row.length > 1 ? row[1].trim() : "";
-      String naam = row.length > 3 ? row[3].trim() : "";
-      double amount =
-          double.tryParse(row.length > 5 ? row[5].trim() : "0") ?? 0.0;
+      if (row.length < 6) continue;
 
-      if (newBalances.containsKey(jama)) {
-        newBalances[jama] = (newBalances[jama] ?? 0.0) + amount;
+      final amount = double.tryParse(row[5].trim()) ?? 0.0;
+      final jama = row[1].trim();
+      final naam = row[3].trim();
+
+      if (accountKeys.contains(jama)) {
+        newBalances[jama] = newBalances[jama]! + amount;
       }
-      if (newBalances.containsKey(naam)) {
-        newBalances[naam] = (newBalances[naam] ?? 0.0) - amount;
+      if (accountKeys.contains(naam)) {
+        newBalances[naam] = newBalances[naam]! - amount;
       }
     }
 
@@ -66,31 +68,35 @@ class _OptionsScreenState extends State<OptionsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(children: [
-          const SizedBox(height: 20),
-
-          // Account Balances Display
-          Card(
-            elevation: 20,
-            color: Colors.blue[50],
-            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Padding(
-              padding: const EdgeInsets.all(5),
-              child: GridView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: accountBalances.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 5, // Adjust to fit your screen
-                  childAspectRatio: 2.9,
-                  crossAxisSpacing: 1,
-                  mainAxisSpacing: 1,
-                ),
-                itemBuilder: (context, index) {
-                  String account = accountBalances.keys.elementAt(index);
-                  double balance = accountBalances[account] ?? 0.0;
-                  return Card(
+      backgroundColor: Colors.white,
+      body: Column(children: [
+        const SizedBox(height: 20),
+        Card(
+          elevation: 20,
+          color: Colors.blue[50],
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Padding(
+            padding: const EdgeInsets.all(5),
+            child: GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: accountBalances.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 5,
+                childAspectRatio: 2.9,
+                crossAxisSpacing: 1,
+                mainAxisSpacing: 1,
+              ),
+              itemBuilder: (context, index) {
+                String account = accountBalances.keys.elementAt(index);
+                double balance = accountBalances[account] ?? 0.0;
+                return InkWell(
+                  onTap: () {
+                    setState(() {
+                      fetchBalances();
+                    });
+                  },
+                  child: Card(
                     color: Colors.white,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(8),
@@ -119,117 +125,117 @@ class _OptionsScreenState extends State<OptionsScreen> {
                         ],
                       ),
                     ),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              // Logo Image
-              SizedBox(
-                height: 300,
-                width: 500,
-                child: Card(
-                  semanticContainer: true,
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  elevation: 5,
-                  margin: const EdgeInsets.all(10),
-                  child: Image.asset(
-                    'assets/logo.jpeg',
-                    fit: BoxFit.fill,
-                  ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              height: 300,
+              width: 500,
+              child: Card(
+                semanticContainer: true,
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                elevation: 50,
+                margin: const EdgeInsets.all(10),
+                child: Image.asset(
+                  'assets/logo.jpeg',
+                  fit: BoxFit.fill,
                 ),
               ),
-
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                          height: 80,
-                          width: 300,
-                          child: Button_Widget(
-                              context, 'Today Cash Book', Colors.black, () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => const Todaycb()));
-                          })),
-                      const SizedBox(width: 10),
-                      SizedBox(
-                          height: 80,
-                          width: 300,
-                          child: Button_Widget(
-                              context, 'General ledger', Colors.black, () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => const DailyCashBook()));
-                          })),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                          height: 80,
-                          width: 300,
-                          child: Button_Widget(
-                            context,
-                            'Gud Bill',
-                            Colors.black,
-                            () {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) => const GudBillScreen(),
-                                ),
-                              );
-                            },
-                          )),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                          height: 80,
-                          width: 300,
-                          child: Button_Widget(
-                            context,
-                            'Khata \'Accounts\'',
-                            Colors.black,
-                            () {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) => const KhataScreen(),
-                                ),
-                              );
-                            },
-                          )),
-                      const SizedBox(width: 10),
-                      SizedBox(
-                          height: 80,
-                          width: 300,
-                          child: Button_Widget(
-                              context, 'Due Payment', Colors.black, () {
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                        height: 80,
+                        width: 300,
+                        child: Button_Widget(
+                            context, 'Today Cash Book', Colors.black, () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const Todaycb()));
+                        })),
+                    const SizedBox(width: 10),
+                    SizedBox(
+                        height: 80,
+                        width: 300,
+                        child: Button_Widget(
+                            context, 'General ledger', Colors.black, () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const DailyCashBook()));
+                        })),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                        height: 80,
+                        width: 300,
+                        child: Button_Widget(
+                          context,
+                          'Gud Bill',
+                          Colors.black,
+                          () {
                             Navigator.of(context).push(
                               MaterialPageRoute(
-                                builder: (context) => const DuePaymentScreen(),
+                                builder: (context) => const GudBillScreen(),
                               ),
                             );
-                          })),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          )
-        ]));
+                          },
+                        )),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                        height: 80,
+                        width: 300,
+                        child: Button_Widget(
+                          context,
+                          'Khata \'Accounts\'',
+                          Colors.black,
+                          () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => const KhataScreen(),
+                              ),
+                            );
+                          },
+                        )),
+                    const SizedBox(width: 10),
+                    SizedBox(
+                        height: 80,
+                        width: 300,
+                        child: Button_Widget(
+                            context, 'Due Payment', Colors.black, () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => const DuePaymentScreen(),
+                            ),
+                          );
+                        })),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        )
+      ]),
+    );
   }
 }
